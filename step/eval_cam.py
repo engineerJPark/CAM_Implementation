eece0ef3ce5490b035416a55f031f91c29d108e5
engineerJPark <jshackist@gmail.com>
2023-01-05 11:31:50 +0900

    fixed eval cam

--- step/eval_cam.py
+++ step/eval_cam.py
@@ -8,9 +8,54 @@ def run(args):
     dataset = VOCSemanticSegmentationDataset(split=args.chainer_eval_set, data_dir=args.voc12_root)
     labels = [dataset.get_example_by_keys(i, (1,))[0] for i in range(len(dataset))]
 
+    print("raw cam")
     preds = []
     for id in dataset.ids:
         cam_dict = np.load(os.path.join(args.cam_out_dir, id + '.npy'), allow_pickle=True).item()
+        cams = cam_dict['high_res']
+        cams = np.pad(cams, ((1, 0), (0, 0), (0, 0)), mode='constant', constant_values=args.cam_eval_thres) # threshold as 0.15
+        keys = np.pad(cam_dict['keys'] + 1, (1, 0), mode='constant')
+        cls_labels = np.argmax(cams, axis=0)
+        cls_labels = keys[cls_labels]
+        preds.append(cls_labels.copy())
+
+    confusion = calc_semantic_segmentation_confusion(preds, labels)
+
+    gtj = confusion.sum(axis=1)
+    resj = confusion.sum(axis=0)
+    gtjresj = np.diag(confusion)
+    denominator = gtj + resj - gtjresj
+    iou = gtjresj / denominator
+
+    print({'iou': iou, 'miou': np.nanmean(iou)})
+
+    ### in crf
+    print("cam after crf"))
+    preds = []
+    for id in dataset.ids:
+        cam_dict = np.load(os.path.join(args.crf_out_dir, id + '.npy'), allow_pickle=True).item()
+        cams = cam_dict['high_res']
+        cams = np.pad(cams, ((1, 0), (0, 0), (0, 0)), mode='constant', constant_values=args.cam_eval_thres) # threshold as 0.15
+        keys = np.pad(cam_dict['keys'] + 1, (1, 0), mode='constant')
+        cls_labels = np.argmax(cams, axis=0)
+        cls_labels = keys[cls_labels]
+        preds.append(cls_labels.copy())
+
+    confusion = calc_semantic_segmentation_confusion(preds, labels)
+
+    gtj = confusion.sum(axis=1)
+    resj = confusion.sum(axis=0)
+    gtjresj = np.diag(confusion)
+    denominator = gtj + resj - gtjresj
+    iou = gtjresj / denominator
+
+    print({'iou': iou, 'miou': np.nanmean(iou)})
+
+    ### in aff
+    print("cam after affinitynet"))
+    preds = []
+    for id in dataset.ids:
+        cam_dict = np.load(os.path.join(args.aff_out_dir, id + '.npy'), allow_pickle=True).item()
         cams = cam_dict['high_res']
         cams = np.pad(cams, ((1, 0), (0, 0), (0, 0)), mode='constant', constant_values=args.cam_eval_thres) # threshold as 0.15
         keys = np.pad(cam_dict['keys'] + 1, (1, 0), mode='constant')
